88646956e42bf1c5270c8c0b0639bdea4cbaae43
Sebastian Siemssen <fubhy@fubhy.com>
2020-08-07 20:33:23 +0200

    feat: use block number and log index

---
+++ src/mappings/feeds.ts
@@ -0,0 +1,31 @@
+import { AnswerUpdated, NewRound } from '../generated/AggregatorInterface';
+import { ensurePriceFeed } from '../entities/PriceFeed';
+import { Price } from '../generated/schema';
+
+export function handleAnswerUpdatedForPair(
+  pair: string,
+  event: AnswerUpdated,
+): void {
+  let feed = ensurePriceFeed(pair);
+  feed.latestTimestamp = event.params.timestamp;
+  feed.latestPrice = event.params.current;
+  feed.save();
+
+  let id =
+    event.address.toHex() +
+    '/' +
+    event.block.number.toString() +
+    '/' +
+    event.logIndex.toString();
+
+  let price = new Price(id);
+  price.feed = feed.id;
+  price.price = event.params.current;
+  price.timestamp = event.params.timestamp;
+  price.round = event.params.roundId;
+  price.save();
+}
+
+export function handleNewRoundForPair(pair: string, event: NewRound): void {
+  // TODO: Do this later.
+}
--- src/mappings/feeds/ethusd.ts
+++ src/mappings/feeds/ethusd.ts
@@ -1,23 +1,10 @@
+import { handleAnswerUpdatedForPair, handleNewRoundForPair } from '../feeds';
 import { AnswerUpdated, NewRound } from '../../generated/AggregatorInterface';
-import { ensurePriceFeed } from '../../entities/PriceFeed';
-import { Price } from '../../generated/schema';
 
 export function handleAnswerUpdated(event: AnswerUpdated): void {
-  let feed = ensurePriceFeed('ETH/USD');
-  feed.latestTimestamp = event.params.timestamp;
-  feed.latestPrice = event.params.current;
-  feed.latestRound = event.params.roundId;
-  feed.save();
-
-  let id = event.address.toHex() + '/' + event.params.roundId.toString();
-  let price = new Price(id);
-  price.feed = feed.id;
-  price.price = event.params.current;
-  price.timestamp = event.params.timestamp;
-  price.round = event.params.roundId;
-  price.save();
+  handleAnswerUpdatedForPair('ETH/USD', event);
 }
 
 export function handleNewRound(event: NewRound): void {
-  // TODO: Do this later.
+  handleNewRoundForPair('ETH/USD', event);
 }
