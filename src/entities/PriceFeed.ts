89a6a6827cbc85fef2b1d007a7b299a6624406f6
Sebastian Siemssen <fubhy@fubhy.com>
2020-08-07 20:29:11 +0200

    feat: monitor ethusd

--- src/entities/PriceFeed.ts
+++ src/entities/PriceFeed.ts
@@ -1,1 +1,17 @@
-// Helpers for interacting with the PriceFeed entity.
+import { BigInt } from '@graphprotocol/graph-ts';
+import { PriceFeed } from '../generated/schema';
+
+export function ensurePriceFeed(pair: string): PriceFeed {
+  let feed = PriceFeed.load(pair) as PriceFeed;
+  if (feed) {
+    return feed;
+  }
+
+  feed = new PriceFeed(pair);
+  feed.latestPrice = BigInt.fromI32(0);
+  feed.latestTimestamp = BigInt.fromI32(0);
+  feed.latestRound = BigInt.fromI32(0);
+  feed.save();
+
+  return feed;
+}
--- src/mappings/feeds/ethusd.ts
+++ src/mappings/feeds/ethusd.ts
@@ -1,5 +1,23 @@
 import { AnswerUpdated, NewRound } from '../../generated/AggregatorInterface';
+import { ensurePriceFeed } from '../../entities/PriceFeed';
+import { Price } from '../../generated/schema';
 
-export function handleAnswerUpdated(event: AnswerUpdated): void {}
+export function handleAnswerUpdated(event: AnswerUpdated): void {
+  let feed = ensurePriceFeed('ETH/USD');
+  feed.latestTimestamp = event.params.timestamp;
+  feed.latestPrice = event.params.current;
+  feed.latestRound = event.params.roundId;
+  feed.save();
 
-export function handleNewRound(event: NewRound): void {}
+  let id = event.address.toHex() + '/' + event.params.roundId.toString();
+  let price = new Price(id);
+  price.feed = feed.id;
+  price.price = event.params.current;
+  price.timestamp = event.params.timestamp;
+  price.round = event.params.roundId;
+  price.save();
+}
+
+export function handleNewRound(event: NewRound): void {
+  // TODO: Do this later.
+}
