9006eaaa090b5a8490ccb7d6c7f1eac03dde0cb3
Sebastian Siemssen <fubhy@fubhy.com>
2020-10-09 10:32:19 +0200

    feat: add tracking of price deviation

--- src/entities/Price.ts
+++ src/entities/Price.ts
@@ -1,3 +1,4 @@
+import { BigInt } from '@graphprotocol/graph-ts';
 import { AnswerUpdated } from '../generated/AggregatorInterface';
 import { Price, PriceFeed } from '../generated/schema';
 import { logCritical } from '../utils/logCritical';
@@ -11,15 +12,26 @@ export function priceId(event: AnswerUpdated): string {
 }
 
 export function createPrice(event: AnswerUpdated, feed: PriceFeed): Price {
+  let previous: Price | null = feed.latestPrice != '' ? usePrice(feed.latestPrice) : null;
+  let deviation: BigInt | null = null;
+
+  if (previous != null) {
+    let difference = event.params.current.minus(previous.price);
+    deviation = difference.times(BigInt.fromI32(100)).div(previous.price);
+  }
+
   let id = priceId(event);
   let price = new Price(id);
   price.blockNumber = event.block.number;
   price.blockHash = event.block.hash.toHex();
   price.transactionHash = event.transaction.hash.toHex();
   price.assetPair = feed.assetPair;
-  price.priceFeed = usePriceFeed(feed.id).id;
+  price.priceFeed = feed.id;
   price.timestamp = event.params.updatedAt;
   price.price = event.params.current;
+  price.priceDeviation = deviation;
+  price.previousPrice = previous != null ? previous.id : null;
+  price.timeSincePreviousPrice = previous != null ? price.timestamp.minus(previous.timestamp) : null;
   price.save();
 
   return price;
